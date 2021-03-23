e967eff580108746648b867a5a52900c3faefdc6
Masa <katsukatsu.masa.0522@gmail.com>
2021-03-24 00:39:27 +0900

    methods内追記

--- main.js
+++ main.js
@@ -27,7 +27,16 @@ var app = new Vue ({
        },
        saveList: function(){
         localStorage.setItem('things', JSON.stringify(this.things));
-       }
+       },
+       loadList: function(){
+        this.things=JSON.parse(localStorage.getItem('things'));
+        if(!this.things){
+            this.things=[];
+        }
+    },
+    mounted: function(){
+        this.loadList();
+    }
    },
    computed: {
         setDate: function() {
