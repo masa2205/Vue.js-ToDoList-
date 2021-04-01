05e69e37dc42c303b631ea8917b8f135b05062c8
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-02 03:08:42 +0900

    deletetodo関数編集

--- main.js
+++ main.js
@@ -1,57 +1,60 @@
 var app = new Vue ({
-   el: "#app",
-   data: {
-       title: "",
-       things: [],
-   },
-   methods: {
-       add: function(){
-           if(this.newthings == "")return;
-           this.things.push({
-               title: this.newthings,
-               isChecked: false,
-           });
-           this.newthings = "";
-           this.saveList();
-       },
-       deleteTodo: function(){
-           result = confirm("本当に終わりましたか？");
-           if(result) {
-              this.things = this.things.filter(function(thing){
-                  return thing.isChecked === false;
-              }),
-              alert("お疲れ様でした!")
-           }else{
-
-           };
-           this.saveList();
-       },
-       saveList: function(){
-        localStorage.setItem('things', JSON.stringify(this.things));
-       },
-       loadList: function(){
-           this.things=JSON.parse(localStorage.getItem('things'));
-           if(!this.things){
-               this.things=[];
-           }
-       },
-       mounted: function(){
-           this.loadList();
-       }
-   },
-   computed: {
-        setDate: function() {
-           hiduke = new Date();
-           year = hiduke.getFullYear();
-           month = hiduke.getMonth()+1;
-           day = hiduke.getDate();
-           return this.transfer_data = year + '/' + month + '/' + day ;
+    el: "#app",
+    data: {
+        title: "",
+        things: [],
+    },
+    methods: {
+        add: function(){
+            if(this.newthings == "")return;
+            this.things.push({
+                title: this.newthings,
+                isChecked: false,
+            });
+            this.newthings = "";
+            this.saveList();
         },
-        remaining: function() {
-            return this.things.filter(function(thing) {
-                return thing.isChecked == true;
-            }).length;
+        deleteTodo: function(){
+          if(this.things.length <= 0){
+ 
+          }else{
+            result = confirm("本当に終わりましたか？");
+            if(result) {
+               this.things = this.things.filter(function(thing){
+                   return thing.isChecked === false;
+               }),
+               alert("お疲れ様でした!")
+            }else{
+ 
+            };
+            this.saveList();
+          }
         },
-
-   }
-})
+        saveList: function(){
+         localStorage.setItem('things', JSON.stringify(this.things));
+        },
+        loadList: function(){
+            this.things=JSON.parse(localStorage.getItem('things'));
+            if(!this.things){
+                this.things=[];
+            }
+        },
+        mounted: function(){
+            this.loadList();
+        }
+    },
+    computed: {
+         setDate: function() {
+            hiduke = new Date();
+            year = hiduke.getFullYear();
+            month = hiduke.getMonth()+1;
+            day = hiduke.getDate();
+            return this.transfer_data = year + '/' + month + '/' + day ;
+         },
+         remaining: function() {
+             return this.things.filter(function(thing) {
+                 return thing.isChecked == true;
+             }).length;
+         },
+    }
+ })
